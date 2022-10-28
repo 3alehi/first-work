b04b2e54b392150d39892bf3aee92c91d1b8b968
AmirHossein <116015277+salehiamir2006@users.noreply.github.com>
2022-10-28 09:49:42 +0330

    Add files via upload

---
+++ menu/a.js
@@ -0,0 +1,12 @@
+const add = document.getElementsByClassName('add')
+
+add[1].addEventListener('click' , ()=>{
+
+    add.forEach(function(index){
+    
+    console.log(index);
+    
+})
+
+
+console.log(add[1]);
